2a23b023b76ada09cfc98497c2bbc4be72486fed
Stepan Novozhilov <hit2hat@icloud.com>
2019-05-13 10:11:57 +0300

    Make `Switch` stateless

--- src/components/Switch/index.js
+++ src/components/Switch/index.js
@@ -1,41 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-class Switch extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            switched: props.switched
-        };
-        this.onSwitch = props.onSwitch;
-    }
-
-    turnTo(switched) {
-        this.setState({ switched });
-        this.onSwitch(switched);
-    }
-
-    render() {
-        const { switched } = this.state;
-        return (
-            <div className={this.props.className ? "switch " + this.props.className : "switch"}>
-                <div
-                    className={switched ? "switch__side switch__toggled" : "switch__side"}
-                    onClick={this.turnTo.bind(this, true)}
-                >
-                    ON
-                </div>
-                <div
-                    className={!switched ? "switch__side switch__toggled" : "switch__side"}
-                    onClick={this.turnTo.bind(this, false)}
-                >
-                    OFF
-                </div>
+const Switch = ({ className, switched, onSwitch }) => {
+    return (
+        <div className={className ? "switch " + className : "switch"}>
+            <div
+                className={switched ? "switch__side switch__toggled" : "switch__side"}
+                onClick={onSwitch.bind(this, true)}
+            >
+                ON
             </div>
-        );
-    }
-}
+            <div
+                className={!switched ? "switch__side switch__toggled" : "switch__side"}
+                onClick={onSwitch.bind(this, false)}
+            >
+                OFF
+            </div>
+        </div>
+    );
+};
 
 Switch.propTypes = {
     /**
