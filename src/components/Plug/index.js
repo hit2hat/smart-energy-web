e2ad6bae68d01f9482619d1babd668275e7a7aae
Stepan Novozhilov <hit2hat@icloud.com>
2019-05-13 10:12:07 +0300

    Make `Plug` stateless

--- src/components/Plug/index.js
+++ src/components/Plug/index.js
@@ -7,56 +7,36 @@ import Switch from "../Switch";
 import PlugOnIcon from "../../assets/plug.svg";
 import PlugOffIcon from "../../assets/plug_off.svg";
 
-class Plug extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            voltage: props.voltage,
-            amperage: props.amperage,
-            power: props.power,
-            isOn: props.isOn,
-            index: props.index,
-            name: props.name
-        };
-    }
+const Plug = ({ voltage, amperage, power, isOn, index, name, className, onTurn }) => {
+    return (
+        <Group className={className ? "plug " + className : "plug"}>
+            <div className="plug-visual">
+                <img src={isOn ? PlugOnIcon : PlugOffIcon} alt=""/>
+                <Switch
+                    className="plug-visual__switch"
+                    switched={isOn}
+                    onSwitch={onTurn}
+                />
+            </div>
+            <div className="plug-info">
+                <div className="plug-info__title"><span>#{index}</span> {name}</div>
+                {
+                    isOn ?
+                        <div className="plug-info__metrics">
+                            <div>Напряжение: <span>{voltage}</span> Вольт</div>
+                            <div>Сила тока: <span>{amperage}</span> Ампера</div>
+                            <div>Мощность: <span>{power}</span> Ватт</div>
+                        </div> :
+                        <div className="plug-info__metrics">
+                            Для отображения статистики включите розетку
+                        </div>
+                }
 
-    onSwitch(switched) {
-        this.setState({ isOn: switched });
-    }
-
-    render() {
-        const { voltage, amperage, power, isOn, index, name } = this.state;
-
-        return (
-            <Group className={this.props.className ? "plug " + this.props.className : "plug"}>
-                <div className="plug-visual">
-                    <img src={isOn ? PlugOnIcon : PlugOffIcon} alt=""/>
-                    <Switch
-                        className="plug-visual__switch"
-                        switched={isOn}
-                        onSwitch={this.onSwitch.bind(this)}
-                    />
-                </div>
-                <div className="plug-info">
-                    <div className="plug-info__title"><span>#{index}</span> {name}</div>
-                    {
-                        isOn ?
-                            <div className="plug-info__metrics">
-                                <div>Напряжение: <span>{voltage}</span> Вольт</div>
-                                <div>Сила тока: <span>{amperage}</span> Ампера</div>
-                                <div>Мощность: <span>{power}</span> Ватт</div>
-                            </div> :
-                            <div className="plug-info__metrics">
-                                Для отображения статистики включите розетку
-                            </div>
-                    }
-
-                </div>
-            </Group>
-        );
-    }
-}
+            </div>
+        </Group>
+    );
+};
 
 Plug.propTypes = {
     /**
@@ -86,7 +66,11 @@ Plug.propTypes = {
     /**
      * Название
      */
-    name: PropTypes.string
+    name: PropTypes.string,
+    /**
+     * Функция, вызываемая при включении или выключении
+     */
+    onTurn: PropTypes.func.isRequired
 };
 
 Plug.defaultProps = {
